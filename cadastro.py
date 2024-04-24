91d2f29c7da890d2aac29a3553138c164777b588
111douglas <129951581+111douglas@users.noreply.github.com>
2024-04-24 09:13:37 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,23 @@
+cadastro1 = input("Digite seu nome: ")
+idade1 = int(input("Digite sua idade: "))
+sexo1 = input("Digite seu sexo: ")
+
+cadastro2 = input("Digite seu nome: ")
+idade2 = int(input("Digite sua idade: "))
+sexo2 = input("Digite seu sexo: ")
+
+cadastro3 = input("Digite seu nome: ")
+idade3 = int(input("Digite sua idade: "))
+sexo3 = input("Digite seu sexo: ")
+
+print("nome:", cadastro1)
+print("idade:", idade1)
+print("sexo:", sexo1)
+
+print("nome:", cadastro2)
+print("idade:", idade2)
+print("sexo:", sexo2)
+
+print("nome:", cadastro3)
+print("idade:", idade3)
+print("sexo:", sexo3)
